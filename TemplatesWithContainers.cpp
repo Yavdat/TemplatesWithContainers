eb7553df4ae39dc89f598646db6565dbaae1abff
Yavdat <yavdat.it@gmail.com>
2022-12-29 23:13:52 +0300

    overloaded map and pair with operator <<

--- TemplatesWithContainers.cpp
+++ TemplatesWithContainers.cpp
@@ -14,10 +14,27 @@ ostream& operator<< (ostream& out, const vector<T>& vi) {
     return out;
 }
 
+template <typename First, typename Second>
+ostream& operator<< (ostream& out, const pair<First, Second>& p) {
+    out << p.first << ',' << p.second;
+    return out;
+}
+
+template <typename Key, typename Value>
+ostream& operator<< (ostream& out, const map<Key, Value>& vi) {
+    for (const auto& i : vi) {
+        out << i << ' ';
+    }
+    return out;
+}
+
 int main() {
     vector<int> vi = {1, 2, 3};
     cout << vi << endl;
     vector<double> vd = {1.5, 3, 4.5};
     cout << vd << endl;
+
+    map<int, int> m = {{1, 2}, {3, 4}};
+    cout << m << endl;
     return 0;
 }
